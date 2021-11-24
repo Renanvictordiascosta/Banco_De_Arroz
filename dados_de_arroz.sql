d1f1238dc41a2fe867d8cd042f351a8d839eb8e2
Renan Victor <80129254+Renanvictordiascosta@users.noreply.github.com>
2021-11-24 14:47:36 -0300

    Add files via upload

---
+++ dados_de_arroz.sql
@@ -0,0 +1,166 @@
+-- Criar a base de dados
+create database arroz;
+
+-- Usar essa base de dados
+use arroz;
+
+-- Criar uma tabela
+create table registro_de_arroz(
+	tipo_de_arroz varchar(50) not null,
+	preco float not null,
+	aparencia varchar(50) not null,
+	calorias int not null
+);
+
+-- Fiz da maneira mais difícil a inserção de dados na tabela
+-- Só para treinar mesmo, e, incrivelmente, foi menos de 10 minutos
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco Amarronzado", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco Amarronzado", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco Amarronzado", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Branco Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Branco Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Branco Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco Amarronzado", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco Amarronzado", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco Amarronzado", 3500);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 5.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 6.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Branco", 7.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Branco Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Amarronzado", 3600);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Branco Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.00, "Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Branco Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 4.50, "Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Branco Amarronzado", 3700);
+
+insert into registro_de_arroz(tipo_de_arroz, preco, aparencia, calorias)
+values("Arroz Integral", 5.00, "Amarronzado", 3700);
+
+-- Mostrar as tabelas criadas
+select tipo_de_arroz from registro_de_arroz;
+select preco from registro_de_arroz;
+select aparencia from registro_de_arroz;
+select calorias from registro_de_arroz;
